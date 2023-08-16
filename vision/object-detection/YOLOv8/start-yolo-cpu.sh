993582271f0a3d62c2b7840bba75b0150856d3b8
smamindl <smamindla@amperecomputing.com>
2023-08-16 18:10:01 +0000

    changing number of threads

--- vision/object-detection/YOLOv8/start-yolo-cpu.sh
+++ vision/object-detection/YOLOv8/start-yolo-cpu.sh
@@ -3,8 +3,9 @@
 export GRADIO_SERVER_NAME="0.0.0.0"
 export GRADIO_SERVER_PORT=5000
 export NSTREAMS=4
-export NTHREADS=16
+export NTHREADS=12
 export HOST_PORT=5000
 
 cont_name="cont-1"
-docker compose -p $cont_name up -d app-yolo-live
+docker compose -p $cont_name  up -d app-yolo-live
+#docker compose -p   $cont_name run -p 5000:5000 --entrypoint /bin/bash app-yolo-live
