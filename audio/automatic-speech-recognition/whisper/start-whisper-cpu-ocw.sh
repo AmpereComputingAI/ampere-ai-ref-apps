6470d65c311850e4625acb1b26aee74507c2df30
Iyappan Subramanian <isubramanian@amperecomputing.com>
2023-09-12 04:39:26 -0700

    Added OCW start script and updated docker tag

---
+++ audio/automatic-speech-recognition/whisper/start-whisper-cpu-ocw.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export GRADIO_SERVER_NAME="0.0.0.0"
+export GRADIO_SERVER_PORT=5001
+export HOST_PORT=5001
+export INSTANCE_NAME='OCI Ampere A1'
+export NSTREAMS=1
+export NTHREADS=16
+
+cont_name="cont-1"
+docker compose -p $cont_name up -d app-whisper
