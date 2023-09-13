d96fbed6c52c9d3559cd336a833ac25ba52c3250
Iyappan Subramanian <isubramanian@amperecomputing.com>
2023-09-13 09:35:42 -0700

    Updated docker tag
    Added ocw start script

---
+++ vision/object-detection/YOLOv8/start-yolo-cpu-ocw.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export GRADIO_SERVER_NAME="0.0.0.0"
+export GRADIO_SERVER_PORT=5000
+export INSTANCE_NAME='OCI Ampere A1'
+export NSTREAMS=1
+export NTHREADS=16
+export HOST_PORT=5000
+
+cont_name="cont-1"
+docker compose -p $cont_name  up -d app-yolo-live
+#docker compose -p   $cont_name run -p 5000:5000 --entrypoint /bin/bash app-yolo-live
